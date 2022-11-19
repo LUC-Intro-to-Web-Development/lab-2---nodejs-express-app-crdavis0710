60135066d7e357069b44ecad2b277dced4470390
Christopher Davis <crdavis0710@gmail.com>
2022-11-19 12:12:47 -0600

    Lab 2 - needs fixing

---
+++ routes/users.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+  let userData = fs.readFileSync('./users.json');
+  var siteUsers = JSON.parse(userData);
+  var createdUsers = siteUsers;
+  res.render('users', {createdUsers});
+});
+
+module.exports = router;
